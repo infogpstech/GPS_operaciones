60726f03f8274665ac4f6e382c2f027ca60f1929
GPSpedia <gpsinfotecno@gmail.com>
2026-06-29 04:35:17 -0600

    Update GOS_CORE API endpoint URL

--- api-config.js
+++ api-config.js
@@ -4,7 +4,7 @@ export const API_ENDPOINTS = {
     WRITE: "https://script.google.com/macros/s/AKfycbwgpSXDpF9jW20mqvK8jP9clxs1Gacz-o6caOEBm1hBweAxkBe1lAW9hJTh1fMFcOvP/exec",
     USERS: "https://script.google.com/macros/s/AKfycbxAqyEcAHetH6yN4qccGILL-L3IzMSPVuVJ1kpuO86GqfDXTKP8cHrrB7UkKN1r_0g5/exec",
     FEEDBACK: "https://script.google.com/macros/s/AKfycbwHTr8MSFuNio8rky8tflcErlRlAb1YSH2jmszZp77SM5e_-SVMO2pBU1UmeGOH1Aig/exec",
-    GOS_CORE: "https://script.google.com/macros/s/AKfycbybs77462gjgOVr8BLBs4svx79IRgR8yJFs2wuLYWuoocYzDbSUx2XK3cRCrMHJvSU/exec"
+    GOS_CORE: "https://script.google.com/macros/s/AKfycbwFgKnq0-HCR77XQ1e96b3nbNQ_6oHEwReYPNYobroySnyDZxMYuebSIqa8Evi7RdKw/exec"
 };
 
 export async function routeAction(service, action, payload = {}) {
